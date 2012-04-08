069e1d2e6664aa009cd26d0b5044d4588498d6c6
Zhusee Zhang <zhusee2@gmail.com>
2012-04-08 15:00:25 +0800

    Add live version for browsers and phones

---
+++ live/scripts/consts.js
@@ -0,0 +1,49 @@
+const cityNameEntities = {
+  "Keelung_City": "&#22522;&#38534;&#24066;",
+  "Taipei_City": "&#33274;&#21271;&#24066;",
+  "New_Taipei_City": "&#26032;&#21271;&#24066;",
+  "Taoyuan_County": "&#26691;&#22290;&#32291;",
+  "Hsinchu_City": "&#26032;&#31481;&#24066;",
+  "Hsinchu_County": "&#26032;&#31481;&#32291;",
+  "Miaoli_County": "&#33495;&#26647;&#32291;",
+  "Taichung_City": "&#33274;&#20013;&#24066;",
+  "Changhua_County": "&#24432;&#21270;&#32291;",
+  "Nantou_County": "&#21335;&#25237;&#32291;",
+  "Yunlin_County": "&#38642;&#26519;&#32291;",
+  "Chiayi_City": "&#22025;&#32681;&#24066;",
+  "Chiayi_County": "&#22025;&#32681;&#32291;",
+  "Tainan_City": "&#33274;&#21335;&#24066;",
+  "Kaohsiung_City": "&#39640;&#38596;&#24066;",
+  "Pingtung_County": "&#23631;&#26481;&#32291;",
+  "Yilan_County": "&#23452;&#34349;&#32291;",
+  "Hualien_County": "&#33457;&#34030;&#32291;",
+  "Taitung_County": "&#33274;&#26481;&#32291;",
+  "Lienchiang_County": "&#36899;&#27743;&#32291;",
+  "Penghu_County": "&#28558;&#28246;&#32291;",
+  "Kinmen_County": "&#37329;&#38272;&#32291;"
+}
+
+const realtimeStationLocation = {
+  "Taipei_City": "46692", //台北
+  "New_Taipei_City": "A0A9M", //板橋
+  "Taichung_City": "46749", //台中
+  "Tainan_City": "46741", //台南
+  "Kaohsiung_City": "46744", //高雄
+  "Keelung_City": "46694", //基隆
+  "Taoyuan_County": "46697", //桃園
+  "Hsinchu_City": "46757", //新竹
+  "Hsinchu_County": "C0D56", //竹東
+  "Miaoli_County": "C0E53", //三義
+  "Changhua_County": "C0G65", //員林
+  "Nantou_County": "C0I11", //竹山
+  "Yunlin_County": "C0K33", //虎尾
+  "Chiayi_City": "46748", //嘉義
+  "Chiayi_County": "C0M41", //大埔
+  "Pingtung_County": "46759", //恆春
+  "Yilan_County": "46708", //宜蘭
+  "Hualien_County": "46699", //花蓮
+  "Taitung_County": "46766", //台東
+  "Penghu_County": "46735", //澎湖
+  "Kinmen_County": "46711", //金門
+  "Lienchiang_County": "46799" //馬祖
+};
